0eed895961b9dc10b1edb1def39adbcb81f1b6e3
tohichoi@naver.com <tohichoi@naver.com>
2026-05-17 02:19:22 +0900

    regenerated parser.c

--- src/parser.c
+++ src/parser.c
@@ -1286,7 +1286,7 @@ extern "C" {
 #define TS_PUBLIC __attribute__((visibility("default")))
 #endif
 
-TS_PUBLIC const TSLanguage *tree_sitter_ranger_rc_conf(void) {
+TS_PUBLIC const TSLanguage *tree_sitter_rangerrc(void) {
   static const TSLanguage language = {
     .abi_version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
@@ -1311,7 +1311,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_ranger_rc_conf(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "ranger_rc_conf",
+    .name = "rangerrc",
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
